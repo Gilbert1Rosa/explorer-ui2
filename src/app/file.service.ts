7584a896548977d7039a41d38b8794d05102c668
Gilbert1Rosa <gilbertli2033@gmail.com>
2022-05-24 12:42:14 -0400

    Adding communication service.

---
+++ src/app/file.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable()
+export class FileService {
+    public constructor(private http: HttpClient) {}
+
+    getChildren(params: any): Observable<Object> {
+        return this.http.get("/api/fileexplorer/object", 
+        { 
+            params: { 
+                folderId: params.folderId
+            } 
+        });
+    }
+}
